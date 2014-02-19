73d8b107f55f4ef3fd07a590159715150283742f
Dominik Kundel <dominik.kundel@gmail.com>
2014-02-19 17:49:13 +0100

    basic bootstrap for sync script

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // Module dependencies.
 var express = require('express');
 var app = express();
+var settings = require('./settings');
 
 // Express Configuration
 require('./lib/config/express')(app);
@@ -12,7 +13,7 @@ require('./lib/routes')(app);
 
 
 // Start server
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || settings.port;
 app.listen(port, function () {
   console.log('Express server listening on port %d in %s mode', port, app.get('env'));
 });
